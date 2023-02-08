4b007b299a6851d6ba1549cad11ef267b35a09a2
Janeth Eni <enijaneth@gmail.com>
2023-02-08 11:04:21 +0000

    Fix pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,13 @@ from datetime import datetime
 
 
 def do_pack():
-     """generating a .tgz archive from the contents of the web_static"""
-     local('mkdir -p versions')
-     time = datetime.now().strftime("%Y%m%d%H%M%S")
-     name = "web_static_{}.tgz".format(time)
-     file_name = "versions/{}".format(name)
-     location = local("tar -cvzf {} web_static/".format(file_name))
-     if location:
-         return (location)
-     else:
-         return None
+    """generating a .tgz archive from the contents of the web_static"""
+    local('mkdir -p versions')
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "web_static_{}.tgz".format(time)
+    file_name = "versions/{}".format(name)
+    location = local("tar -cvzf {} web_static/".format(file_name))
+    if location:
+        return (location)
+    else:
+        return None
